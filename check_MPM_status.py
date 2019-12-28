ae0a09d2486030a2de4d9ddb7cfa66901380facb
hsinghan <hsinghan1102@gmail.com>
2019-12-28 22:48:17 +0800

    Check_MPM_status_v1.2
    Update code structure.

--- check_MPM_status.py
+++ check_MPM_status.py
@@ -2,34 +2,45 @@ import os
 # import subprocess
 import win32api
 from tkinter import messagebox
-import linecache
 import tkinter
+
+Unlock_Status = False
+Lock_Status   = True
 
 def show_Message(msg):
     # hide main window
     root = tkinter.Tk()
     root.withdraw()
-    messagebox.showinfo("Notification", msg)
+    messagebox.showinfo("Notification",  msg)
 
 def is_MPM_Lock(file_path):
-    msg = linecache.getline(file_path, 108)
-    print(msg)
-    if '*Unlock' in msg:
-        return False
+    with open(file_path) as f:
+        content = f.readlines()
+
+    index = content.index('Manufacturing Programming Mode\n')
+
+    result = "\tUnlock\n" in content[index+1]
+
+    if result == Lock_Status:
+        return Lock_Status
     else:
-        return True
+        return Unlock_Status
+
 
 if __name__ == '__main__':
     # get file path
-    # BCU_path = os.getcwd() + '\\BiosConfigUtility64.exe'
-    # myCmd = "BiosConfigUtility64.exe /get:"BIOS_Setting.txt"
-    # os.system(myCmd)
-    #MPM_txt_path = os.getcwd() + '\\BIOS_Setting.txt'
+    BCU_path = os.getcwd() + '\\BiosConfigUtility64.exe'
+    myCmd = "BiosConfigUtility64.exe /get:"BIOS_Setting.txt" "
+    os.system(myCmd)
+    MPM_txt_path = os.getcwd() + '\\BIOS_Setting.txt'
 
-    if is_MPM_Lock('MU.txt') :
-        show_Message("The unit is MPM Lock status!")
+    if is_MPM_Lock(MPM_txt_path) :
+        MPM_txt_path = os.getcwd() + '\\BIOS_Setting.txt'
+        os.remove(MPM_txt_path)
+        show_Message("The unit status in MPM Lock.")
     else:
-        show_Message("The unit is MPM UnLock status!")
+        MPM_txt_path = os.getcwd() + '\\BIOS_Setting.txt'
+        os.remove(MPM_txt_path)
+        show_Message("The unit status in MPM UnLock.")
 
-    #MPM_txt_path = os.getcwd() + '\\BIOS_Setting.txt'
-    #os.remove(MPM_txt_path)
+
